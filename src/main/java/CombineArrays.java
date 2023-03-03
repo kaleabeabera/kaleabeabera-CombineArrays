a585da055901c11cdb2383bb3ffc9ae8b89ad836
kaleabeabera <Kabera@colgate.edu>
2023-03-03 08:47:49 +0000

    Completed Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -4,11 +4,24 @@ public class CombineArrays {
      * Combine two arrays into a single array and return it.
      *
      * @param arr1 an array of ints.
-     * @param arr2 an array of ints.
+     * @param arr2 an array of ints. 
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int[] result = new int[arr1.length + arr2.length];
+        int index = 0;
+        
+        for (int i = 0; i < arr1.length; i++) {
+            result[index] = arr1[i];
+            index++;
+        }
+        
+        for (int j = 0; j < arr2.length; j++) {
+            result[index] = arr2[j];
+            index++;
+        }
+        
+        return result;
     }
 }
